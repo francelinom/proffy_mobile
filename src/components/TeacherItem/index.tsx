7dfca4c02c9b23c02e94a060de79aaaa44fbe5af
Francelino <fmarcilio1989@gmail.com>
2020-08-25 22:21:00 -0300

    estilo finalizado

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { View, Image, Text } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+
+import heartOutlineIcon from '../../assets/images/icons/heart-outline.png';
+import unFavoriteIcon from '../../assets/images/icons/unfavorite.png';
+import whatsappIcon from '../../assets/images/icons/whatsapp.png';
+
+import styles from './styles';
+
+function TeacherItem(){
+    return (
+        <View style={styles.container}>
+            <View style={styles.profile}>
+                <Image
+                    style={styles.avatar}
+                    source={{ uri: 'https://github.com/francelinom.png' }}
+                />
+                <View style={styles.profileInfo}>
+                    <Text style={styles.name}>Francelino Marcílio</Text>
+                    <Text style={styles.subject}>Programação</Text>
+                </View>
+            </View>
+
+            <Text style={styles.bio}>
+                Aluno de Analise e Desenvolvimento de Sistemas
+                {'\n'}{'\n'}
+                Pensamento em Cursar uma Pós-Graduação 
+            </Text>
+
+            <View style={styles.footer}>
+                <Text style={styles.price}>
+                    Preço/hora {'   '}
+                    <Text style={styles.priceValue}>R$ 100,00</Text>
+                </Text>
+
+                <View style={styles.buttonsContainer}>
+                    <RectButton style={[styles.favoriteButton, styles.favorited]}>
+                        <Image source={heartOutlineIcon} />
+                        <Image source={unFavoriteIcon} />
+                    </RectButton>
+
+                    <RectButton style={styles.contactButton}>
+                        <Image source={whatsappIcon} />
+                        <Text style={styles.contactButtonText}>Entrar em contato</Text>
+                    </RectButton>
+                </View>
+            </View>
+        </View>
+    )
+}
+
+export default TeacherItem;
--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ScrollView } from 'react-native';
 
 import PageHeader from '../../components/PageHeader';
 
 
 import styles from './styles';
+import TeacherItem from '../../components/TeacherItem';
 
 function Favorites() {
     return (
         <View style={styles.container}>
             <PageHeader title="Mus Proffys Favoritos" />
+
+            <ScrollView 
+                style={styles.teacherList}
+                contentContainerStyle={{
+                    paddingHorizontal: 16,
+                    paddingBottom: 16,
+                }}
+            >
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+            </ScrollView>
         </View>
     );
 }
